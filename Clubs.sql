33a47d20c2717decd4d02953d6c8425d361ac0c5
Richard <86606594+Richard-JWE@users.noreply.github.com>
2021-06-30 05:12:41 +0000

    Add files via upload

---
+++ Clubs.sql
@@ -0,0 +1,5 @@
+Insert into clubs 
+Values             ('Manchester Utd' , 'Old Trafford' , 74140 , 'Ole Gunnar Solskjaer' , '1878-01-01');
+
+Insert into clubs 
+Values ('Liverpool FC', 'Anfield', '53394', 'Jurgen Klopp', '1892-06-03');
